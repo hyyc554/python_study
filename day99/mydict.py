a4e964f2d49ce8bf9b19c6a1e5410f2eeca3fa72
hyc <hyc554@outlook.com>
2018-10-19 11:18:29 +0800

    单元测试

---
+++ day99/mydict.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class Dict(dict):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+
+    def __getattr__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % item)
+
+    def __setattr__(self, key, value):
+        self[key]=value
+
+
