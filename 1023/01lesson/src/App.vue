7f277017643eb89bd9c5691b263aa982ec939efd
hyc <hyc554@outlook.com>
2018-10-23 21:58:44 +0800

    vue-conponent

---
+++ 1023/01lesson/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Vheader></Vheader>
+    
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+import Vheader from './components/Vheader'
+  export default {
+    name:'app',
+    data() {
+      return {
+        msg: 'hello yc'
+      }
+    },
+    components:{
+      Vheader
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
